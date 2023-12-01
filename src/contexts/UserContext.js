652d30b1d25c54a4eb67e95163c730afb6c9f9f2
James-Lee-01 <freebike01@gmail.com>
2023-12-01 11:36:30 +0800

    feat: add user id info

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,23 +1,32 @@
 // UserContext.js
 import { createContext, useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [username, setUsername] = useState(sessionStorage.getItem("chatUsername") || "");
+  const [userId, setUserId] = useState(sessionStorage.getItem("chatUserId") || "");
+
 
   const login = (name) => {
+    const newUserId = uuidv4();
     sessionStorage.setItem("chatUsername", name);
+    sessionStorage.setItem("chatUserId", newUserId);
     setUsername(name);
+    setUserId(newUserId);
   };
 
   const logout = () => {
     sessionStorage.removeItem("chatUsername");
+    sessionStorage.removeItem("chatUserId");
     setUsername("");
+    setUserId("");
   };
 
   return (
-    <UserContext.Provider value={{ username, login, logout }}>
+    <UserContext.Provider value={{ username, userId, login, logout }}>
       {children}
     </UserContext.Provider>
   );
